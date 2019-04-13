fe6325a8da2e26bfd6be2097ff4c8535430df776
ryotaro.isaki <mdask8ryotaro@gmail.com>
2019-04-13 03:02:14 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+   include SessionsHelper
+
+
   private
   def require_user_logged_in
     unless logged_in?
